80238e0d7c229a087d8830471b969d61423fa34a
katebry <katebryan31@gmail.com>
2022-02-15 09:41:41 -0800

    chore: added DarcyWeisbach param types

---
+++ src/utils/DarcyWeisbachTypes.ts
@@ -0,0 +1,24 @@
+// Pipe length
+export type Pipelength = {
+  L: number;
+};
+
+// Friction factor
+export type FrictionFactor = {
+  f: number;
+};
+
+// Fluid density
+export type FluidDensity = {
+  rho: number;
+};
+
+// Pipe diameter
+export type PipeDiameter = {
+  D: number;
+};
+
+// Fluid velocity
+export type FluidVelocity = {
+  v: number;
+};
