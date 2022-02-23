1bee187542130d848359fc8bc2e1f59c4e336c27
katebry <katebryan31@gmail.com>
2022-02-23 20:16:13 +0000

    feat: beginning of page layout (css grid) implemented

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <h1>Pressure Drop</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+  text-align: center;
+}
+</style>
