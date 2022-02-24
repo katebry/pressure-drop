8f9f011061ac4035719b90230952b3c36c7771a0
katebry <katebryan31@gmail.com>
2022-02-24 08:13:52 +0000

    chore: added units to InputData fields

--- src/components/InputData.vue
+++ src/components/InputData.vue
@@ -3,18 +3,22 @@
     <div class="row">
       Density
       <input type="number" />
+      kg/m3
     </div>
     <div class="row">
       Dynamic viscosity
       <input type="number" />
+      kg/ms
     </div>
     <div class="row">
       Pipe roughness
       <input type="number" />
+      mm
     </div>
     <div class="row">
       Flowrate
       <input type="number" />
+      l/min
     </div>
   </div>
 </template>
