8ef892c5bbee7d7a64ffa5e0bd588a4f0c79c9a1
katebry <katebryan31@gmail.com>
2022-03-10 14:36:32 +0000

    feat: beginning of table names/fields added

--- src/components/PipeRouteTable.vue
+++ src/components/PipeRouteTable.vue
@@ -3,13 +3,27 @@
   <table id="secondTable">
     <thead>
       <tr>
-        <th v-for="col in columns" :key="col.id">{{ col }}</th>
+        <th v-for="col in columnTitles" :key="col.heading">
+          {{ col.heading }}
+        </th>
       </tr>
     </thead>
     <tbody>
-      <tr v-for="(row, key) in rows" :key="key">
-        <td v-for="col in columns" :key="col.id">{{ row[col] }}</td>
-      </tr>
+    <tr>
+      <td><input class='item_no_input' type='number' id='item_no1' name='item_no1' value=1></td>
+      <td><select name='item_type1' id='item_type1'>
+        <option value='Pipe'>Pipe</option>
+        <option value='Ball Valve'>Ball Valve</option>
+        <option value='Globe Valve'>Globe Valve</option>
+        <option value='Bend'>Bend</option>
+        <option value='Tee'>Tee</option>
+      </select></td>
+      <td><input class='inputs' type='number' id='diameter1' name='diameter1' value=0.025>mm</td>
+      <td><input class='inputs' type='number' id='pipe_length1' name='pipe_length1' value=1>m</td>
+      <td><input class='NAfield' type='number' id='K_factor1' name='K_factor1' readonly></td>
+      <td><input class='inputs' type='number' id='pdrop1' name='pdrop1' value=25>Pa</td>
+      <td><input class='outputs' type='number' id='pdrop_total1' name='pdrop_total1' readonly/>Pa</td>
+    </tr>
     </tbody>
   </table>
 </template>
@@ -18,38 +32,18 @@
 export default {
   data() {
     return {
-      rows: [
-        {
-          id: 1,
-          name: "Chandler Bing",
-          phone: "305-917-1301",
-          profession: "IT Manager",
-        },
-        {
-          id: 2,
-          name: "Ross Geller",
-          phone: "210-684-8953",
-          profession: "Paleontologist",
-        },
-        {
-          id: 3,
-          name: "Rachel Green",
-          phone: "765-338-0312",
-          profession: "Waitress",
-        },
-        {
-          id: 4,
-          name: "Monica Geller",
-          phone: "714-541-3336",
-          profession: "Head Chef",
-        },
-        {
-          id: 5,
-          name: "Joey Tribbiani",
-          phone: "972-297-6037",
-          profession: "Actor",
-        },
+      columnTitles: [
+        { heading: "Item No." },
+        { heading: "Item Type" },
+        { heading: "Diameter" },
+        { heading: "Pipe Length" },
+        { heading: "Fitting K Factor" },
+        { heading: "Item Pressure Drop" },
+        { heading: "Pressure Drop Total" },
       ],
+      rows: [
+        {}
+      ]
     };
   },
   computed: {
